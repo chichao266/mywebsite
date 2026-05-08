69ac28ec861eed9f2e9829f25bd6cb414c0461ff
Store Owner <store@example.com>
2026-05-08 16:12:29 +0800

    Use serverComponentsExternalPackages under experimental

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@prisma/client", "prisma", "@base-ui/react"],
+  experimental: {
+    serverComponentsExternalPackages: ["@base-ui/react"],
+  },
 };
 
 export default nextConfig;
